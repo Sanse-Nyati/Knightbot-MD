d4a2c1c4249f21fed160d255a5c955eb8d23a11c
Andrew <afandeandrew639@gmail.com>
2025-06-02 22:52:55 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Andrew Junior', // Your name
-  ownerNumber: '254116128538', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '254784489022', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
